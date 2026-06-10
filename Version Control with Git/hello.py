1bada07abdfe4961677785ae52b81dc8b085ded1
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-09 21:03:51 -0700

    Create hello.py

---
+++ Version Control with Git/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A simple hello world program
+"""
+
+def main():
+    print("Hello, Git World!")
+    print("This is my first versioned program.")
+
+if __name__ == "__main__":
+    main()
